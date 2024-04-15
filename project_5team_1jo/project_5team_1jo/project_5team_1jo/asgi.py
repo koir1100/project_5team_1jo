604b664c916a6751a668cdd673b25ab8a3201599
Yonggu Choi <koir1100@gmail.com>
2024-04-15 15:02:26 +0900

    Initial Commit

---
+++ project_5team_1jo/project_5team_1jo/project_5team_1jo/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for project_5team_1jo project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_5team_1jo.settings')
+
+application = get_asgi_application()
