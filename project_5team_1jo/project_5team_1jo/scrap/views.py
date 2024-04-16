2e72b01c374e352cea9e0296b2037f13aa777ca7
caindy <38023693+caindy-P@users.noreply.github.com>
2024-04-16 15:05:42 +0900

    make templates

--- project_5team_1jo/project_5team_1jo/scrap/views.py
+++ project_5team_1jo/project_5team_1jo/scrap/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+from bs4 import BeautifulSoup
 
 def index(request):
-    return HttpResponse('Hello')
+    key = "b4f9749d8859ee4e5584a3ecae413c4400466b92c3bf48c71f19b390eae80663"
+    startNum = 1
+    endNum = 1
+    startDate = 20200101
+    endDate = 20200110
+    drcode = 11
+    res = requests.get("https://nl.go.kr/NL/search/openApi/saseoApi.do?key=%s&startRowNumApi=%d&endRowNumApi=%d&start_date=%d&end_date=%d&drcode=%d" % (key, startNum, endNum, startDate, endDate, drcode))
+    soup = BeautifulSoup(res.text, "lxml")
+    return render(request, 'scrap/index.html')
