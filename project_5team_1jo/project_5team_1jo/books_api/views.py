1d19997cf05661dd7b0bcff7ad746dbcf0b007b2
caindy <38023693+caindy-P@users.noreply.github.com>
2024-04-18 18:36:24 +0900

    make restAPI BookSpecific
    특정 drcode의 책만 가져오는 BookSpecific restAPI를 만들었습니다.

--- project_5team_1jo/project_5team_1jo/books_api/views.py
+++ project_5team_1jo/project_5team_1jo/books_api/views.py
@@ -1,6 +1,9 @@
 from books.models import RecomBooks
 from books_api.serializer import RecomBooksSerializer
 from rest_framework import generics, permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 #from . import permissions as api_permissions
 
 #POST(생성), GET(로드)
@@ -14,3 +17,10 @@ class BookDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = RecomBooks.objects.all()
     serializer_class = RecomBooksSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+class BookSpecific(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request, code):
+        books = RecomBooks.objects.filter(drcode=code)
+        serializer = RecomBooksSerializer(books, many = True)
+        return Response(serializer.data)
