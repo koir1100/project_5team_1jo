ef56604e7bee8dabd8f0ba569927f977391555f3
Yonggu Choi <koir1100@gmail.com>
2024-04-20 06:40:46 +0900

    fix. directory structure relocation

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Workspace\project_5team_1jo\project_5team_1jo
+set VIRTUAL_ENV=C:\Workspace\project_5team_1jo
 
 if not defined PROMPT set PROMPT=$P$G
 
